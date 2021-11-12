f26b83d772ee3008fdb67b03ec8fbd47afbbaa0a
Manny Verma <mverma7845@gmail.com>
2021-11-11 18:42:52 -0700

    working calculator

--- main.js
+++ main.js
@@ -5,51 +5,108 @@ class Calculator {
         this.currentOperand = currentOperand
         this.clear()
     }
-
+    
     // Different functions the Calculator needs to do
     clear() {
         // this is the number in the currentOperand
         this.cOperand = ''
-         // this is the number in the previousOperand
+        // this is the number in the previousOperand
         this.pOperand = ''
         // no operations selected since everything was cleared
         this.operation = undefined
     }
 
     delete() {
-    
+        this.cOperand = this.cOperand.toString().slice(0,-1)
     }
 
     //we have to make this strings so javascript doesn't try to add the numbers. This will make sure that numbers are added if we push 1 and 2 we will get 12 in the display not 1 then 2.  The if statement will check to see if a . is added if it is already added it will stop the function. It will not let the user the period (.) more than one time.
     appendNum(number) {
-        if(number === '.' && this.cOperand.includes('.')) return
+        if (number === '.' && this.cOperand.includes('.')) return
         this.cOperand = this.cOperand.toString() + number.toString()
     }
 
     chooseOperation(operation) {
-        // this sets the operation, it sets the operation to the operation that was passed in.
+        if(this.cOperand === '') return
+    // this sets the operation, it sets the operation to the operation that was passed in.
+        if (this.pOperand !== '') {
+            this.compute()
+        }
         this.operation = operation
-        //the previous operand eqauls to the current operand
+     //the previous operand eqauls to the current operand
         this.pOperand = this.cOperand
-        // this clears out the current operand
+    // this clears out the current operand
         this.cOperand = ''
     }
 
     compute() {
+        let computation
+        const prev = parseFloat(this.pOperand)
+        const current = parseFloat(this.cOperand)
+        // this is going to check if previous or current are not inputted then it is going to cancel the operation
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '*':
+                computation = prev * current
+                break
+            case '+':
+                computation = prev + current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.cOperand = computation
+        this.operation = undefined
+        this.pOperand = ''
+    }
 
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDigits = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
     }
 
     updateDisplay() {
-        this.currentOperand.innerText = this.cOperand
+        this.currentOperand.innerHTML =
+            this.getDisplayNumber(this.cOperand)
+        if (this.operation != null) {
+            this.previousOperand.innerHTML =
+                `${this.getDisplayNumber(this.pOperand)} ${this.operation}`
+        } else {
+            this.previousOperand.innerHTML = ''
+        }
     }
 }
+
 const numberButtons = document.querySelectorAll('[data-number]')
-const operationsButtons = document.querySelectorAll('[data-operation]')
+const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperand = document.querySelector('[data-previous-operand]')
 const currentOperand = document.querySelector('[data-current-operand]')
+
+
 
 // defines a calculator class and passes everything from the constructor in to it. The previousOperand and the currentOperand text
 
@@ -59,14 +116,30 @@ const calculator = new Calculator(previousOperand, currentOperand)
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        calculator.appendNum(button.innerText);
+        calculator.appendNum(button.innerHTML);
+        // console.log(button.innerHTML);
         calculator.updateDisplay()
     })
 })
 
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
-        calculator.chooseOperation(button.innerText);
+        calculator.chooseOperation(button.innerHTML);
         calculator.updateDisplay()
     })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
 })
